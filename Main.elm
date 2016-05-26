05ad4157e137980225ffb10440e606971ebe9aca
Josh Adams <josh.rubyist@gmail.com>
2016-05-26 14:19:45 -0500

    Finished with 006.3

--- Main.elm
+++ Main.elm
@@ -33,7 +33,7 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  squares (model.position.x, model.position.y)
+  shapes (model.position.x, model.position.y)
     |> Element.toHtml
 
 
@@ -52,34 +52,63 @@ subscriptions model =
   Mouse.moves MoveMouse
 
 
-squares : (Int, Int) -> Element
-squares (x, y) =
+shapes : (Int, Int) -> Element
+shapes (x, y) =
   let
     theGroup =
       group
-        [ move (0,-55) blueSquare
-        , move (0, 55) redSquare
+        [ move (   0, -55) blueSquare
+        , move (   0,  55) redSquare
+        , move (-110, -55) blueCircle
+        , move (-110,  55) redCircle
+        , move ( 110, -55) blueHexagon
+        , move ( 110,  55) redPentagon
         ]
 
     originGroup =
       move (-400, 400) theGroup
 
     movedGroup =
-        move (toFloat(x), toFloat(-y)) originGroup
+        move (toFloat(x), toFloat(-1 * y)) originGroup
   in
     collage 800 800 [ movedGroup ]
 
 
+blueHexagon : Form
+blueHexagon =
+  filled blue (ngon 6 50)
+
+
+redPentagon : Form
+redPentagon =
+  filled red (ngon 5 50)
+
+
 blueSquare : Form
 blueSquare =
-  traced (dashed blue) square
+  outlined (dashed blue) square
 
 
 redSquare : Form
 redSquare =
-  traced (solid red) square
+  outlined (solid red) square
 
 
-square : Path
+square : Shape
 square =
-  path [ (50,50), (50,-50), (-50,-50), (-50,50), (50,50) ]
+  Collage.square 100
+
+
+blueCircle : Form
+blueCircle =
+  filled blue circle
+
+
+redCircle : Form
+redCircle =
+  filled red circle
+
+
+circle : Shape
+circle =
+  Collage.circle 50
